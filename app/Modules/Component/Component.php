60eeefd5e2eb70802875cb148a8cd98d2f4231a4
Jewel <cep.jewel@gmail.com>
2021-02-08 15:46:19 +0600

    added max selection items

--- app/Modules/Component/Component.php
+++ app/Modules/Component/Component.php
@@ -525,6 +525,7 @@ class Component
                 'loadingText' => __('Loading...', 'fluentform'),
                 'noChoicesText' => __('No choices to choose from', 'fluentform'),
                 'itemSelectText' => __('Press to select', 'fluentform'),
+                'maxItemTextLang' => 'Only %%maxItemCount%% values can be added'
             ]
         ));
 
