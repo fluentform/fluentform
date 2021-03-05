1ca72b5cbf971a3700a9a70f6e4b22a8e210ccbe
Jewel <cep.jewel@gmail.com>
2021-03-06 00:19:57 +0600

    Update Container.php

--- framework/Foundation/Container.php
+++ framework/Foundation/Container.php
@@ -207,10 +207,16 @@ class Container implements ArrayAccess
     protected function resolveClass(ReflectionParameter $parameter)
     {
         $types = ['bool', 'int', 'float', 'string', 'array', 'resource'];
-        
+
         try {
             if ($class = $this->getParameterType($parameter)) {
-                $class = $this->getParameterName($class);
+
+                if($class instanceof \ReflectionType) {
+                    $class = (string) $class;
+                    return $this->make($class);
+                }
+
+                $class = $class->getName();
                 if ($class && in_array($class, $types)) {
                     $class = null;
                 }
@@ -248,22 +254,6 @@ class Container implements ArrayAccess
         }
 
         return $parameter->getClass();
-    }
-
-    /**
-     * Get the parameter name for the given parameter.
-     *
-     * @return string
-     */
-    protected function getParameterName($parameter)
-    {
-        $parameterType = $this->getParameterType($parameter);
-
-        if (property_exists($parameterType, 'name')) {
-            return $parameterType->name;
-        }
-
-        return $parameterType->getName();
     }
 
     /**
