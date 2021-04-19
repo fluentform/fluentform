a8c33bea4f0958bf9854ab342e02067f929656b0
Lukman Nakib <alex@wpmanageninja.com>
2021-04-19 15:20:08 +0600

    Added reCaptcha
    Added reCaptcha v3 separately

--- app/Services/FormBuilder/Components/Recaptcha.php
+++ app/Services/FormBuilder/Components/Recaptcha.php
@@ -14,11 +14,20 @@ class Recaptcha extends BaseComponent
 	{
         $elementName = $data['element'];
         $data = apply_filters('fluenform_rendering_field_data_'.$elementName, $data, $form);
-
+        $key  = get_option('_fluentform_reCaptcha_details');
+       
+        if(\FluentForm\Framework\Helpers\ArrayHelper::get ($key,'api_version') == 'v3_invisible'){
+            return $this->renderV3($data,$form,$key);
+        }
+        if($key && isset($key['siteKey'])) {
+            $siteKey = $key['siteKey'];
+        } else {
+            $siteKey = '';
+        }
         wp_enqueue_script(
 			'google-recaptcha',
 			'https://www.google.com/recaptcha/api.js',
-			array(),
+			 array(),
 			FLUENTFORM_VERSION,
 			true
 		);
@@ -29,12 +38,12 @@ class Recaptcha extends BaseComponent
 		} else {
 			$siteKey = '';
 		}
-
+  
 		$recaptchaBlock = "<div
-		data-sitekey='{$siteKey}'
-		id='fluentform-recaptcha-{$form->id}'
-		class='ff-el-recaptcha g-recaptcha'
-		data-callback='fluentFormrecaptchaSuccessCallback'></div>";
+            data-sitekey='{$siteKey}'
+            id='fluentform-recaptcha-{$form->id}'
+            class='ff-el-recaptcha g-recaptcha'
+            data-callback='fluentFormrecaptchaSuccessCallback'></div>";
 
 		$label = '';
 		if (!empty($data['settings']['label'])) {
@@ -47,5 +56,38 @@ class Recaptcha extends BaseComponent
 		);
 		$html = "<div class='ff-el-group' {$atts}>{$label}{$el}</div>";
         echo apply_filters('fluenform_rendering_field_html_'.$elementName, $html, $data, $form);
+    }
+    
+    private function renderV3($data, $form, $reCaptchaData)
+    {
+        $elementName = $data['element'];
+        $key         = $reCaptchaData['siteKey'];
+        $src         = "https://www.google.com/recaptcha/api.js?render={$key}&onload=ff_onload_recaptcha_v3_callback";
+        add_action ( 'wp_footer', function () use ($src, $key, $form) {
+            ?>
+            <script src="<?php echo $src; ?>" async defer></script>
+            <script type="text/javascript">
+                function ff_onload_recaptcha_v3_callback() {
+                    grecaptcha.ready(function () {
+                        grecaptcha.execute('<?php echo $key; ?>', {
+                            action : 'fluentform/_<?php echo $form->id; ?>'
+                        }).then(function (token) {
+                            var forms = jQuery('form.frm-fluent-form');
+                            forms.find('#ff_recaptcha_v3_<?php echo $form->id; ?>').html('<input type="hidden" name="g-recaptcha-response" value="' + token + '" />');
+                        });
+                    });
+                }
+            
+                jQuery(document).on('reInitExtras', '.<?php echo $form->instance_css_class; ?>', function () {
+                    ff_onload_recaptcha_v3_callback();
+                });
+            </script>
+    
+        <?php }, 11 );
+        
+        $html        = "<div class='ff-el-input--content ff-el-is-error ff-el-recaptcha g-recaptcha ' id='ff_recaptcha_v3_{$form->id}'></div>";
+        echo apply_filters ( 'fluenform_rendering_field_html_' . $elementName, $html, $data, $form );
+        
+
     }
 }
--- resources/assets/admin/settings/reCaptcha.vue
+++ resources/assets/admin/settings/reCaptcha.vue
@@ -2,24 +2,45 @@
     <div>
         <el-row class="setting_header">
             <el-col :md="24">
-                <h2>Google reCAPTCHA Settings (V2)</h2>
+                <h2>Google reCAPTCHA Settings </h2>
 
                 <p>
-                    Fluent Forms integrates with reCAPTCHA, a free service that protects your website from spam and
+                    Fluent Forms integrates with both reCAPTCHA v2 and v3, a free service that protects your website from spam and
                     abuse. Please note, these settings are required only if you decide to use the reCAPTCHA field.
 
                     <a href="http://www.google.com/recaptcha/" target="_blank">
                         Read more about reCAPTCHA.
                     </a>
                 </p>
-                <p><b>Please generate API key and API secret using reCaptcha Version 2</b></p>
+                <p><b>Please generate API key and API secret using reCaptcha Version 2 or 3</b></p>
             </el-col>
         </el-row>
-        
-        
-        <div class="section-body">
-            <el-form label-width="205px" label-position="left">
 
+
+        <div class="section-body">
+            <el-form label-width="205px" label-position="left" v-loading="loading">
+                <!--reCaptcha version-->
+                <el-form-item>
+                    <template slot="label">
+                        reCAPTCHA Type
+                        <el-tooltip class="item" placement="bottom-start" effect="light">
+                            <div slot="content">
+                                <h3>reCAPTCHA Type</h3>
+                                <p>
+                                    Select your reCAPTCHA version. if you do not have <br>
+                                    it yet you can register for one at the provided link. <br>
+                                    reCAPTCHA is a free service.
+                                </p>
+                            </div>
+
+                            <i class="el-icon-info el-text-info"></i>
+                        </el-tooltip>
+                    </template>
+                    <el-radio-group v-model="reCaptcha.api_version"  @change="load">
+                        <el-radio label="v2_visible" >V2 Visible</el-radio>
+                        <el-radio label="v3_invisible" >V3 InVisible</el-radio>
+                    </el-radio-group>
+                </el-form-item>
                 <!--Site key-->
                 <el-form-item>
                     <template slot="label">
@@ -64,13 +85,10 @@
                 </el-form-item>
 
                 <!--Validate Keys-->
-                <el-form-item label="Validate Keys" v-if="siteKeyChanged">
+                <el-form-item label="Validate Keys" v-if="siteKeyChanged && reCaptcha.api_version == 'v2_visible'">
                     <div class="g-recaptcha" id="reCaptcha" :data-sitekey="reCaptcha.siteKey"></div>
                 </el-form-item>
 
-                <el-form-item label="Validate Keys" v-if="siteKeyChanged">
-                    <div class="g-recaptcha" id="reCaptcha2" :data-sitekey="reCaptcha.siteKey"></div>
-                </el-form-item>
 
                 <el-form-item>
                     <el-button
@@ -91,7 +109,7 @@
                     >Save Settings</el-button>
                 </el-form-item>
             </el-form>
-            
+
             <div v-if="reCaptcha_status">
                 <p>Your reCAPTCHA is valid</p>
             </div>
@@ -108,12 +126,14 @@
                 reCaptcha: {
                     siteKey: '',
                     secretKey: '',
+                    api_version: ''
                 },
                 reCaptcha_status: false,
                 siteKeyChanged: false,
                 disabled: false,
                 saving: false,
-                clearing: false
+                clearing: false,
+                loading:false
             }
         },
         methods: {
@@ -126,21 +146,41 @@
                     this.disabled = true;
                     this.siteKeyChanged = true;
                 }
-                
-                this.$nextTick(() => {
-                    let id = 'reCaptcha';
-                    let siteKey = this.reCaptcha.siteKey;
-                    let $reCaptcha = jQuery('#' + id);
-                    $reCaptcha.html('');
-                    window.___grecaptcha_cfg.clients = {};
-                    grecaptcha.render(id, {
-                        'sitekey': siteKey,
-                        'callback': (token) => {
-                            this.reCaptcha.token = token;
-                            this.disabled = false;
-                        }
-                    });
-                })
+                this.loadScript();
+
+                let siteKey = this.reCaptcha.siteKey;
+                switch (this.reCaptcha.api_version) {
+                    case 'v2_visible':
+                        this.$nextTick(() => {
+
+                            let id = 'reCaptcha';
+                            let $reCaptcha = jQuery('#' + id);
+                            $reCaptcha.html('');
+                            window.___grecaptcha_cfg.clients = {};
+                            grecaptcha.render(id, {
+                                'sitekey' : siteKey,
+                                'callback' : (token) => {
+                                    this.reCaptcha.token = token;
+                                    this.disabled = false;
+                                }
+                            });
+
+                        });
+                        break;
+
+                    case 'v3_invisible':
+                        let that = this;
+                        setTimeout(() => {
+                            grecaptcha.execute(siteKey, {
+                                action : 'ff_submit/admin'
+                            }).then(function (token) {
+                                that.reCaptcha.token = token;
+                                that.disabled = false;
+                            })
+                        }, 1000);
+
+                        break;
+                }
             },
             save() {
                 if (! this.validate()) {
@@ -207,10 +247,11 @@
                 return ! ! (this.reCaptcha.siteKey && this.reCaptcha.secretKey);
             },
             getReCaptchaSettings() {
+                this.loading = true;
                 FluentFormsGlobal.$get({
                     action: 'fluentform-global-settings',
                     key: [
-                        '_fluentform_reCaptcha_details', 
+                        '_fluentform_reCaptcha_details',
                         '_fluentform_reCaptcha_keys_status'
                     ]
                 })
@@ -222,16 +263,35 @@
                     this.reCaptcha = recaptcha;
                     this.reCaptcha_status = response.data._fluentform_reCaptcha_keys_status;
 
+                })
+                .always(()=>{
+                  this.loading = false;
                 });
+            },
+            loadScript(){
+                let recaptchaScript = document.createElement('script');
+                let reCAPTCHA_site_key = this.reCaptcha.siteKey;
+
+                if(this.reCaptcha.api_version == 'v3_invisible' ){
+                    recaptchaScript.setAttribute('src', 'https://www.google.com/recaptcha/api.js?render='+reCAPTCHA_site_key);
+
+                }else{
+                    recaptchaScript.setAttribute('src', 'https://www.google.com/recaptcha/api.js');
+                }
+                //remove previous script and add new one
+                document.getElementById("ff_admin_recaptcha_script").innerHTML = "";
+                document.getElementById("ff_admin_recaptcha_script").appendChild(recaptchaScript);
+
             }
         },
         mounted() {
             this.getReCaptchaSettings();
+            this.loadScript();
         },
         created() {
-            let recaptchaScript = document.createElement('script');
+            let recaptchaScript = document.createElement('div');
 
-            recaptchaScript.setAttribute('src', 'https://www.google.com/recaptcha/api.js');
+            recaptchaScript.id= 'ff_admin_recaptcha_script';
 
             document.body.appendChild(recaptchaScript);
         },
