ab0c3a5c9827321cc7bc41fe65cddb748deb5564
Jewel <cep.jewel@gmail.com>
2021-06-01 10:48:21 +0600

    Landing Page asset loading done

--- resources/views/public/conversational-form.php
+++ resources/views/public/conversational-form.php
@@ -8,6 +8,7 @@
     <title><?php echo esc_html($meta['title']); ?></title>
     <meta name="description" content="<?php echo strip_tags($meta['description']) ?>">
     <?php wp_site_icon(); ?>
+    <?php do_action('fluentform_conversational_frame_head'); ?>
     <style id="ffc_generated_css" type="text/css">
         <?php echo $generated_css; ?>
     </style>
@@ -15,62 +16,13 @@
         <?php
             echo $submit_css;
         ?>
+        
     </style>
 
     <style type="text/css">
-        body {
-            height: 100%;
-            width: 100%;
-            overflow: hidden;
-            margin: 0;
-            padding: 0;
-            max-width: 100vw;
-            font-family: sans-serif;
-        }
-        html, body, div, span, applet, object, iframe,
-        h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-        a, abbr, acronym, address, big, cite, code,
-        del, dfn, em, img, ins, kbd, q, s, samp,
-        small, strike, strong, sub, sup, tt, var,
-        b, u, i, center,
-        dl, dt, dd, ol, ul, li,
-        fieldset, form, label, legend,
-        table, caption, tbody, tfoot, thead, tr, th, td,
-        article, aside, canvas, details, embed,
-        figure, figcaption, footer, header, hgroup,
-        menu, nav, output, ruby, section, summary,
-        time, mark, audio, video {
-            margin: 0;
-            padding: 0;
-            border: 0;
-            font-size: 100%;
-            font: inherit;
-            vertical-align: baseline;
-        }
-        /* HTML5 display-role reset for older browsers */
-        article, aside, details, figcaption, figure,
-        footer, header, hgroup, menu, nav, section {
-            display: block;
-        }
-        body {
-            line-height: 1;
-        }
-        body * {
-            box-sizing: border-box;
-        }
-        ol, ul {
-            list-style: none;
-        }
-        blockquote, q {
-            quotes: none;
-        }
-        blockquote:before, blockquote:after,
-        q:before, q:after {
-            content: '';
-            content: none;
-        }
-        .ff_conv_app {
-            font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+        body{height:100%;width:100%;overflow:hidden;margin:0;padding:0;max-width:100vw;font-family:sans-serif}a,abbr,acronym,address,applet,article,aside,audio,b,big,blockquote,body,canvas,caption,center,cite,code,dd,del,details,dfn,div,dl,dt,em,embed,fieldset,figcaption,figure,footer,form,h1,h2,h3,h4,h5,h6,header,hgroup,html,i,iframe,img,ins,kbd,label,legend,li,mark,menu,nav,object,ol,output,p,pre,q,ruby,s,samp,section,small,span,strike,strong,sub,summary,sup,table,tbody,td,tfoot,th,thead,time,tr,tt,u,ul,var,video{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}body{line-height:1}body *{box-sizing:border-box}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:after,blockquote:before,q:after,q:before{content:'';content:none}
+        .ff_conv_app{
+            font-family:system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Fira Sans","Droid Sans","Helvetica Neue",sans-serif;
         }
     </style>
 </head>
@@ -81,7 +33,8 @@
     </div>
 </div>
 <?php
-wp_footer();
+//wp_footer();
+do_action('fluentform_conversational_frame_footer');
 ?>
 </body>
 </html>
