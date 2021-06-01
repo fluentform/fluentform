8c3c9d8b0d5307baad7c1cd1924fbaa97e31ab50
Jewel <cep.jewel@gmail.com>
2021-06-01 12:15:47 +0600

    Added conversational form

--- resources/views/public/conversational-form.php
+++ resources/views/public/conversational-form.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <head>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
@@ -22,7 +21,6 @@
     </style>
     <style type="text/css">
         <?php echo $submit_css; ?>
-
     </style>
 </head>
 <body class="ff_conversation_page_body  ff_conversation_page_<?php echo $form_id; ?>">
