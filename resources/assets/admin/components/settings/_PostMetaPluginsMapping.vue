7808f254027eff58c40b1ad9dfba7601ec5e596b
Jewel <cep.jewel@gmail.com>
2021-06-20 19:48:10 +0600

    added metaboxes

---
+++ resources/assets/admin/components/settings/_PostMetaPluginsMapping.vue
@@ -0,0 +1,175 @@
+<template>
+<div class="post_meta_plugins_mappings">
+    <div class="meta_fields_mapping">
+        <strong style="font-size: 18px;">{{ labels.section_title }}</strong>
+    </div>
+    <hr style="clear:both;margin:20px 0;">
+    <div>
+        <strong style="font-size: 14px;">General Fields</strong>
+        <el-button
+            type="primary"
+            size="mini"
+            icon="el-icon-plus"
+            class="pull-right"
+            @click="addMapping('general_settings')"
+        >Add Another General Field
+        </el-button>
+    </div>
+    <br />
+    <table v-if="general_settings && general_settings.length" class="ff-table">
+        <thead>
+        <tr>
+            <th>{{ labels.remote_label }}</th>
+            <th>{{ labels.local_label }}</th>
+            <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(mapField,index) in general_settings" :key="index">
+            <td>
+                <el-select v-model="mapField.field_key" size="mini" placeholder="Select Field">
+                    <el-option v-for="(field, fieldKey) in general_fields" :key="fieldKey"
+                               :label="field.label" :value="fieldKey"></el-option>
+                </el-select>
+            </td>
+            <td>
+                <inputPopover
+                    fieldType="text"
+                    :data="editorShortcodes"
+                    v-model="mapField.field_value"
+                />
+            </td>
+            <td>
+                <el-button
+                    type="danger"
+                    size="mini"
+                    icon="el-icon-close"
+                    style="margin-top:4px;"
+                    @click="deleteMapping('general_settings',index)"
+                />
+            </td>
+        </tr>
+        </tbody>
+    </table>
+    <div v-else class="no-mapping-alert">
+        There is no mapping found for General Meta fields.
+    </div>
+
+    <template>
+        <hr style="clear:both;margin:30px 0;">
+        <div>
+            <strong style="font-size: 14px;">Advanced Fields</strong>
+            <el-button
+                type="primary"
+                size="mini"
+                icon="el-icon-plus"
+                class="pull-right"
+                @click="addAdvancedMetaFieldMapping()"
+            >Add Another Advanced Field
+            </el-button>
+        </div>
+        <br />
+        <table v-if="addAdvancedMetaFieldMapping && advanced_settings && advanced_settings.length" class="ff-table">
+            <thead>
+            <tr>
+                <th>{{ labels.remote_label }}</th>
+                <th>{{ labels.local_label }}</th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(mapField,index) in advanced_settings" :key="index">
+                <td>
+                    <el-select @change="mapField.field_value = ''" v-model="mapField.field_key"
+                               size="mini" placeholder="Select Field">
+                        <el-option v-for="(field, fieldKey) in advanced_fields"
+                                   :key="fieldKey" :label="field.label" :value="fieldKey"></el-option>
+                    </el-select>
+                </td>
+                <td>
+                    <p v-if="!mapField.field_key">Select {{ labels.remote_label }} First</p>
+                    <template v-else>
+                        <el-select v-model="mapField.field_value" placeholder="Select Form Field"
+                                   clearable>
+                            <el-option
+                                v-for="(formField,fieldName) in getFilteredFields(mapField.field_key)"
+                                :key="fieldName" :value="fieldName"
+                                :label="formField.admin_label"></el-option>
+                        </el-select>
+                        <small v-if="advanced_fields[mapField.field_key]">{{advanced_fields[mapField.field_key].help_message}}</small>
+                    </template>
+
+                </td>
+                <td>
+                    <el-button
+                        type="danger"
+                        size="mini"
+                        icon="el-icon-close"
+                        style="margin-top:4px;"
+                        @click="deleteMapping('advanced_settings', index)"
+                    />
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        <div v-else class="no-mapping-alert">
+            There is no advanced field mapping for this section.
+        </div>
+    </template>
+
+</div>
+</template>
+
+<script type="text/babel">
+import inputPopover from '../input-popover.vue';
+import each from "lodash/each";
+
+export default {
+    name: 'PostMetaPluginMapping',
+    props: [
+        'general_settings',
+        'advanced_settings',
+        'labels',
+        'general_fields',
+        'advanced_fields',
+        'editorShortcodes',
+        'form_fields'
+    ],
+    components: {
+        inputPopover
+    },
+    methods: {
+        addMapping(type) {
+            this[type].push({
+                field_key: '',
+                field_value: ''
+            });
+        },
+        deleteMapping(type, index) {
+            this[type].splice(index, 1);
+        },
+        addAdvancedMetaFieldMapping() {
+            if(!this.advanced_settings) {
+                this.$set(this, 'advanced_settings', []);
+            }
+            this.advanced_settings.push({
+                field_key: '',
+                field_value: ''
+            });
+        },
+        getFilteredFields(fieldKey) {
+            let settingFields = this.advanced_fields[fieldKey];
+            if (!settingFields) {
+                return {};
+            }
+            const fields = {};
+            each(this.form_fields, (item, itemName) => {
+                if (settingFields.acceptable_fields.indexOf(item.element) !== -1) {
+                    fields[itemName] = item;
+                }
+            });
+            return fields;
+        }
+    }
+}
+</script>
