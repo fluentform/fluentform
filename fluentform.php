7b3e6634a47595efe309ce57ff893516ea9283ef
Jewel <cep.jewel@gmail.com>
2021-08-19 21:00:36 +0600

    added translations

--- fluentform.php
+++ fluentform.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Fluent Forms
 Description: Contact Form By Fluent Forms is the advanced Contact form plugin with drag and drop, multi column supported form builder plugin
-Version: 4.1.51
+Version: 4.1.52
 Author: Contact Form - WPManageNinja LLC
 Author URI: https://fluentforms.com
 Plugin URI: https://wpmanageninja.com/wp-fluent-form/
@@ -16,7 +16,7 @@ defined('ABSPATH') or die;
 defined('FLUENTFORM') or define('FLUENTFORM', true);
 define('FLUENTFORM_DIR_PATH', plugin_dir_path(__FILE__));
 
-defined('FLUENTFORM_VERSION') or define('FLUENTFORM_VERSION', '4.1.51');
+defined('FLUENTFORM_VERSION') or define('FLUENTFORM_VERSION', '4.1.52');
 
 if (!defined('FLUENTFORM_HAS_NIA')) {
     define('FLUENTFORM_HAS_NIA', true);
