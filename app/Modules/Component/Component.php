9bcf49e0f12357106dc744a85c98bdda89a25d2b
Lukman Nakib <59053748+alex-authlab@users.noreply.github.com>
2021-08-23 11:17:40 +0600

    Fix - ChoiceJS Vars
    maxItemTextLang was a unknown config item ,changed to maxItemText

--- app/Modules/Component/Component.php
+++ app/Modules/Component/Component.php
@@ -530,7 +530,7 @@ class Component
                 'loadingText' => __('Loading...', 'fluentform'),
                 'noChoicesText' => __('No choices to choose from', 'fluentform'),
                 'itemSelectText' => __('Press to select', 'fluentform'),
-                'maxItemTextLang' => __('Only %%maxItemCount%% values can be added', 'fluentform')
+                'maxItemText' => __('Only %%maxItemCount%% values can be added', 'fluentform'),
             ]
         ));
 
