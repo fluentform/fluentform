86d6844df1d3bf4d8feef39cc319a927a72c7a72
Mahmudul Hasan <mahmudulhasanarif@gmail.com>
2022-02-07 08:52:40 +0600

    Adds built-in login form functionality

--- app/Hooks/Common.php
+++ app/Hooks/Common.php
@@ -47,6 +47,9 @@ $component->addFluentformSubmissionInsertedFilter();
 $component->addIsRenderableFilter();
 $component->registerInputSanitizers();
 
+// Handles built-in login form
+(new \FluentForm\App\Modules\Form\LoginHandler)->init();
+
 add_action('wp', function () use ($app) {
     // @todo: We will remove the fluentform_pages check from April 2021
     if ((isset($_GET['fluent_forms_pages']) || isset($_GET['fluentform_pages']))) {
