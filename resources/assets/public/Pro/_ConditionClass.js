f159456d5007cea388947967fa7f535ad6e30383
Habibur Rahman Delwar <hrdelwar75@gmail.com>
2022-06-14 15:45:11 +0600

    support formatted regex pattern and fix empty pattern match

--- resources/assets/public/Pro/_ConditionClass.js
+++ resources/assets/public/Pro/_ConditionClass.js
@@ -100,10 +100,27 @@ class ConditionApp {
         } else if (item.operator == 'doNotContains') {
             return val !== null && val.indexOf(item.value) == -1;
         } else if(item.operator == 'test_regex') {
-            const globalRegex = new RegExp(item.value, 'g');
+            const globalRegex = this.stringToRegex(item.value);
+
+            // for regex pattern whice match empty value
+            if (!val && globalRegex.test('')) return true;
             return  globalRegex.test(val);
         }
         return false;
+    }
+
+    stringToRegex(regex) {
+        let {
+            body,
+            flags,
+        } = String(regex)
+            .match(/^\/(?<body>.*)\/(?<flags>[gimsuy]*)$/)
+            ?.groups || {};
+        if (body) {
+            flags = flags ? flags : 'g';
+            return RegExp(body, flags);
+        }
+        return new RegExp(regex, 'g');
     }
 }
 
