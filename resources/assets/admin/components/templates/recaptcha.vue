a3d9d20ee120e75f8e0d51aeddfbeb572bef0a47
Habibur Rahman Delwar <hrdelwar75@gmail.com>
2022-08-16 12:13:13 +0600

    fix ReCaptcha hCaptcha label Placement

--- resources/assets/admin/components/templates/recaptcha.vue
+++ resources/assets/admin/components/templates/recaptcha.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-form-item :label="item.settings.label">
+    <el-form-item :label="item.settings.label" :class="{['ff-el-form-'+item.settings.label_placement]: item.settings.label_placement }">
         <img style="max-width: 60px;" :src="plugin_url+'img/recaptcha-placeholder.png'" alt="recaptcha-placeholder">
     </el-form-item>
 </template>
