4fffb3105c9ad451374204d8a87035bb39be03f0
Lukman Nakib <48018765+nkb-bd@users.noreply.github.com>
2022-09-30 15:13:54 +0600

    Hide label placement settings when label is empty

--- resources/assets/admin/components/editor-field-settings/templates/radioButton.vue
+++ resources/assets/admin/components/editor-field-settings/templates/radioButton.vue
@@ -1,8 +1,8 @@
 <template>
-<el-form-item>
+<el-form-item v-if="show">
     <elLabel slot="label" :label="listItem.label" :helpText="listItem.help_text"></elLabel>
     <el-radio-group size="mini" v-model="model">
-        <el-radio-button v-for="(opt, i) in listItem.options" :key="opt.value" :label="opt.value">{{ opt.label }}</el-radio-button>
+        <el-radio-button v-for="opt in listItem.options" :key="opt.value" :label="opt.value">{{ opt.label }}</el-radio-button>
     </el-radio-group>
 </el-form-item>
 </template>
@@ -17,11 +17,27 @@ export default {
     watch: {
         model() {
             this.$emit('input', this.model);
+        },
+        "$attrs.editItem.settings.label": function (newVal, oldVal) {
+          if (newVal === '') {
+            this.show = false;
+            this.$emit('input', '');
+          } else {
+            this.show = true;
+            this.$emit('input', this.model);
+          }
         }
     },
     data() {
         return {
+            show : true,
             model: this.value
+        }
+    },
+    mounted() {
+        if ('label' in this.$attrs.editItem.settings && !this.$attrs.editItem.settings.label) {
+          this.show = false;
+          this.$emit('input', '');
         }
     }
 }
