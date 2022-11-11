13f188d5b3650ecceff806b08c4267f932028882
Mahmudul Hasan <mahmudulhasanarif@gmail.com>
2022-11-11 18:13:20 +0600

    Releases v4.3.22

--- fluentform.php
+++ fluentform.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Fluent Forms
 Description: Contact Form By Fluent Forms is the advanced Contact form plugin with drag and drop, multi column supported form builder plugin
-Version: 4.3.21
+Version: 4.3.22
 Author: Contact Form - WPManageNinja LLC
 Author URI: https://fluentforms.com
 Plugin URI: https://wpmanageninja.com/wp-fluent-form/
@@ -15,7 +15,7 @@ defined('ABSPATH') or die;
 
 defined('FLUENTFORM') or define('FLUENTFORM', true);
 define('FLUENTFORM_DIR_PATH', plugin_dir_path(__FILE__));
-defined('FLUENTFORM_VERSION') or define('FLUENTFORM_VERSION', '4.3.21');
+defined('FLUENTFORM_VERSION') or define('FLUENTFORM_VERSION', '4.3.22');
 
 if (!defined('FLUENTFORM_HAS_NIA')) {
     define('FLUENTFORM_HAS_NIA', true);
