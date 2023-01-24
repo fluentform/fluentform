9b5d0bcd3d1668999d398afb324f544b38d0a4ff
Kamran <kamran.techydevs@gmail.com>
2023-01-24 18:00:49 +0600

    working on support

--- resources/views/admin/smtp/index.php
+++ resources/views/admin/smtp/index.php
@@ -1,8 +1,8 @@
 <?php do_action('fluentform_global_menu'); ?>
 <div class="ff_addon_wrapper ff_block">
-    <div class="ff_addon_header">
-        <img style="width: 100px; height: 100px;" src="<?php echo esc_url($logo); ?>"/>
-        <h1>Making Email Deliverability Easy and Fast for Your WordPress</h1>
+    <div class="ff_addon_header text-center">
+        <img class="mb-3" style="width: 100px; height: 100px;" src="<?php echo esc_url($logo); ?>"/>
+        <h1 class="mb-4">Making Email Deliverability Easy and Fast for Your WordPress</h1>
         <p>
             <a target="_blank" href="https://wordpress.org/plugins/fluent-smtp/" rel="nofollow">FluentSMTP</a> is the ultimate WP Mail Plugin that connects with your Email Service Provider natively and makes
             sure your emails are delivered 💯.
@@ -11,62 +11,54 @@
             Made by Fluent Forms team for you.
         </p>
         <?php if (!$is_installed): ?>
-            <div class="ff_addon_btn_wrapper">
+            <div class="ff_addon_btn_wrapper mb-6 mt-6">
                 <button class="ff_addon_btn intstall_fluentsmtp">Install and Activate FluentSMTP Plugin</button>
                 <p style="display: none;" class="ff_addon_installing">Installing FluentSMTP Plugin. Please wait...</p>
             </div>
         <?php else: ?>
-        <h3>FluentSMTP plugin has been activated and running</h3>
-        <br />
+        <h3 class="mb-3">FluentSMTP plugin has been activated and running</h3>
         <a href="<?php echo esc_url($setup_url); ?>" class="ff_addon_btn">View FluentSMTP Settings</a>
         <?php endif; ?>
-
     </div>
+
     <?php if (!$is_installed): ?>
-        <div class="ff_addon_body">
-            <img class="ff_addon_banner" src="<?php echo esc_url($banner_image); ?>"/>
-        </div>
-        <div class="ff_addon_feature_card ff_block_box">
-            <div class="ff_addon_header_ff">FluentSMTP - The ultimate SMTP & Email Service Connection Plugin for
-                WordPress
-            </div>
-            <div class="ff_addon_body">
-                <p>
-                    For FluentCRM users, we built a well-optimized SMTP/Amazon SES plugin. It will help you manage all
-                    your
-                    WordPress website emails, including FluentCRM emails. <a target="_blank" rel="noopener"
-                                                                             href="https://fluentsmtp.com/">Learn more
-                        about
-                        FluentSMTP Plugin</a>.
+        <div class="ff_addon_body ff_card">
+            <img class="ff_addon_banner mb-5" src="<?php echo esc_url($banner_image); ?>"/>
+            <div class="ff_card_head">
+                <h4 class="title">FluentSMTP - The ultimate SMTP & Email Service Connection Plugin for WordPress</h4>
+                <p class="text">
+                    For FluentCRM users, we built a well-optimized SMTP/Amazon SES plugin. It will help you manage all your
+                    WordPress website emails, including FluentCRM emails. 
+                    <a target="_blank" rel="noopener" href="https://fluentsmtp.com/">Learn more about FluentSMTP Plugin</a>.
                 </p>
-                <div class="ff_addon_features">
-                    <div class="ff_block_half">
-                        <h3>Dedicated API and SMTP connections</h3>
-                        <ul>
-                            <li>Amazon SES</li>
-                            <li>Mailgun</li>
-                            <li>SendGrid</li>
-                            <li>SendInBlue</li>
-                            <li>PepiPost</li>
-                            <li>SparkPost</li>
-                            <li>+ Any SMTP Provider</li>
-                        </ul>
-                    </div>
-                    <div class="ff_block_half">
-                        <h3>Features of Fluent SMTP Plugin</h3>
-                        <ul>
-                            <li>Optimized API connection with Mail Service Providers</li>
-                            <li>Email Logging for better visibility</li>
-                            <li>Email Routing based on the sender email address</li>
-                            <li>Real-Time Email Delivery</li>
-                            <li>Resend Any Emails</li>
-                            <li>In Details Reporting</li>
-                            <li>Super fast UI powered by VueJS</li>
-                        </ul>
-                    </div>
-                </div>
             </div>
-            <div class="ff_addon_footer text-center">
+            <div class="el-row mt-5" style="margin-left: -12px; margin-right: -12px;">
+                <div class="el-col el-col-12" style="padding-left: 12px; padding-right: 12px;">
+                    <h5 class="mb-4">Dedicated API and SMTP connections</h5>
+                    <ul class="fs-15">
+                        <li class="mb-3"><i class="el-icon el-icon-success text-success mr-2"></i> Amazon SES</li>
+                        <li class="mb-3"><i class="el-icon el-icon-success text-success mr-2"></i> Mailgun</li>
+                        <li class="mb-3"><i class="el-icon el-icon-success text-success mr-2"></i> SendGrid</li>
+                        <li class="mb-3"><i class="el-icon el-icon-success text-success mr-2"></i> SendInBlue</li>
+                        <li class="mb-3"><i class="el-icon el-icon-success text-success mr-2"></i> PepiPost</li>
+                        <li class="mb-3"><i class="el-icon el-icon-success text-success mr-2"></i> SparkPost</li>
+                        <li class="mb-3"><i class="el-icon el-icon-success text-success mr-2"></i> + Any SMTP Provider</li>
+                    </ul>
+                </div>
+                <div class="el-col el-col-12" style="padding-left: 12px; padding-right: 12px;">
+                    <h5 class="mb-4">Features of Fluent SMTP Plugin</h5>
+                    <ul class="fs-15">
+                        <li class="mb-3"><i class="el-icon el-icon-success text-success mr-2"></i> Optimized API connection with Mail Service Providers</li>
+                        <li class="mb-3"><i class="el-icon el-icon-success text-success mr-2"></i> Email Logging for better visibility</li>
+                        <li class="mb-3"><i class="el-icon el-icon-success text-success mr-2"></i> Email Routing based on the sender email address</li>
+                        <li class="mb-3"><i class="el-icon el-icon-success text-success mr-2"></i> Real-Time Email Delivery</li>
+                        <li class="mb-3"><i class="el-icon el-icon-success text-success mr-2"></i> Resend Any Emails</li>
+                        <li class="mb-3"><i class="el-icon el-icon-success text-success mr-2"></i> In Details Reporting</li>
+                        <li class="mb-3"><i class="el-icon el-icon-success text-success mr-2"></i> Super fast UI powered by VueJS</li>
+                    </ul>
+                </div>
+            </div><!-- .el-row -->
+            <div class="ff_addon_footer text-center mt-6 mb-4">
                 <button class="ff_addon_btn ff_invert intstall_fluentsmtp">Install and Activate FluentSMTP Plugin</button>
                 <p style="display: none;" class="ff_addon_installing">Installing FluentSMTP Plugin. Please wait...</p>
             </div>
