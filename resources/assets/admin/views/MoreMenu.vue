ad431a917895f1a4d339d631d61af25162fbcd3d
Kamran <kamran.techydevs@gmail.com>
2023-04-29 16:39:42 +0600

    Fix field delete modal overlay

--- resources/assets/admin/views/MoreMenu.vue
+++ resources/assets/admin/views/MoreMenu.vue
@@ -1,65 +1,65 @@
 <template>
     <div :class="{ ff_backdrop: visible }">
-        <el-dropdown @command="handle" trigger="click">
-            <span class="el-dropdown-link">
-                <i class="ff-icon ff-icon-more-vertical"/>
-            </span>
+        <div class="ff_more_menu">
+            <el-dropdown @command="handle" trigger="click">
+                <span class="el-dropdown-link">
+                    <i class="ff-icon ff-icon-more-vertical"/>
+                </span>
 
-            <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item command="conversational">
-                    {{ convertBtnText }}
-                </el-dropdown-item>
-            </el-dropdown-menu>
-        </el-dropdown>
+                <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item command="conversational">
+                        {{ convertBtnText }}
+                    </el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+
+            <el-dialog
+                :visible.sync="visible"
+                :append-to-body="true"
+                width="60%"
+            >
+                <div slot="title">
+                    <h5 class="mb-2">{{$t('Confirmation')}}</h5>
+                    <p>{{ $t('Are you sure you want to convert this form?') }}</p>
+                </div>
+                
+                <template v-if="!is_conversion_form">
+                    <el-alert
+                        class="mt-4"
+                        title="Warning"
+                        type="warning"
+                        :description="$t('Conversational Forms currently doesn\'t support the following fields: You may also lose data of these fields.')"
+                        show-icon
+                        :closable="false"
+                    >
+                    </el-alert>
+                    <el-row :gutter="20" class="mt-5">
+                        <el-col :span="8" v-for="(field, i) in fields" :key="i">
+                            <div class="mb-3">
+                                <i class="el-icon el-icon-caret-right"></i>
+                                <span>{{ field }}</span>
+                            </div>
+                        </el-col>
+                    </el-row>
+                </template>
 
 
-        <el-dialog
-            :visible.sync="visible"
-            :append-to-body="true"
-            width="60%"
-        >
-            <div slot="title">
-                <h5 class="mb-2">{{$t('Confirmation')}}</h5>
-                <p>{{ $t('Are you sure you want to convert this form?') }}</p>
-            </div>
-            
-            <template v-if="!is_conversion_form">
-                <el-alert
-                    class="mt-4"
-                    title="Warning"
-                    type="warning"
-                    :description="$t('Conversational Forms currently doesn\'t support the following fields: You may also lose data of these fields.')"
-                    show-icon
-                    :closable="false"
-                >
-                </el-alert>
-                <el-row :gutter="20" class="mt-5">
-                    <el-col :span="8" v-for="(field, i) in fields" :key="i">
-                        <div class="mb-3">
-                            <i class="el-icon el-icon-caret-right"></i>
-                            <span>{{ field }}</span>
-                        </div>
-                    </el-col>
-                </el-row>
-            </template>
-
-
-            <span slot="footer" class="dialog-footer">
-                <el-button @click="visible = false" type="info" class="el-button--soft">
-                    {{ $t('Cancel') }}
-                </el-button>
-                <el-button type="primary" icon="el-icon-success" @click="confirm">
-                    {{ $t('Convert') }}
-                </el-button>
-            </span>
-        </el-dialog>
+                <span slot="footer" class="dialog-footer">
+                    <el-button @click="visible = false" type="info" class="el-button--soft">
+                        {{ $t('Cancel') }}
+                    </el-button>
+                    <el-button type="primary" icon="el-icon-success" @click="confirm">
+                        {{ $t('Convert') }}
+                    </el-button>
+                </span>
+            </el-dialog>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: "MoreMenu",
-
     data() {
         return {
             form_id: window.FluentFormApp.form_id,
