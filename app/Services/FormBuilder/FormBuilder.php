e697a855dd6fe1b4133465c4c85e5f7f15e8ccf9
Shahjahan Jewel <cep.jewel@gmail.com>
2023-07-16 13:39:35 +0600

    Update FormBuilder.php

--- app/Services/FormBuilder/FormBuilder.php
+++ app/Services/FormBuilder/FormBuilder.php
@@ -540,7 +540,7 @@ class FormBuilder
     private function fieldsetHtml($form)
     {
         return '<fieldset style="border: none!important;margin: 0!important;padding: 0!important;background-color: transparent!important;box-shadow: none!important;outline: none!important; min-inline-size: 100%;">
-                    <legend class="ff_screen_reader_title" style="display: block; margin: 0!important;padding: 0!important;height: 0!important;text-indent: -999999px;width: 0!important;">'
+                    <legend class="ff_screen_reader_title" style="display: block; margin: 0!important;padding: 0!important;height: 0!important;text-indent: -999999px;width: 0!important;overflow:hidden;">'
                             .$form->title.
                     '</legend>';
     }
