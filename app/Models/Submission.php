35080c1fd41b877d79dc77bfc5f358b1a73137c6
csesumonpro <csesumonpro@gmail.com>
2023-12-22 16:54:55 +0600

    Fix : perpage & page number in submission model
    #377

--- app/Models/Submission.php
+++ app/Models/Submission.php
@@ -150,7 +150,9 @@ class Submission extends Model
     {
         $formId = Arr::get($attributes, 'form_id');
         $query = $this->customQuery($attributes);
-        $response = $query->paginate();
+        $perPage = Arr::get($attributes, 'per_page');
+        $page = Arr::get($attributes, 'page');
+        $response = $query->paginate($perPage, ['*'], 'page', $page);
         $response = apply_filters_deprecated(
             'fluentform_get_raw_responses',
             [
