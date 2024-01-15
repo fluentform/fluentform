23ea6a8862c1de2584939556e43ea65a6831cc9b
Lukman Nakib <lukman.nakib@gmail.com>
2024-01-15 10:32:26 +0600

    Update pro button link

--- resources/assets/admin/components/modals/ImportEntriesModal.vue
+++ resources/assets/admin/components/modals/ImportEntriesModal.vue
@@ -107,7 +107,7 @@
                         <p class="text">{{ $t('Upgrade to get access to import entries.') }}</p>
                     </div>
                     <a target="_blank"
-                       href="https://fluentforms.com/pricing/?utm_source=plugin&amp;utm_medium=wp_install&amp;utm_campaign=ff_upgrade&amp;theme_style=twentytwentythree"
+                       href="https://fluentforms.com/pricing/?utm_source=plugin&amp;utm_medium=wp_install&amp;utm_campaign=ff_upgrade"
                        class="el-button el-button--danger el-button--small">
                         {{ $t('Upgrade to Pro') }}
                     </a>
