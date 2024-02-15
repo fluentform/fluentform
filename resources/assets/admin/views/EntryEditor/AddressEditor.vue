a2fd5731ae53d380f0e6123b234aa09ee9026202
Habibur Rahman Delwar <hrdelwar75@gmail.com>
2024-02-15 16:00:05 +0600

    Fix: repeater/name/address entry edit
    Fix: repeater/name/address entry edit

--- resources/assets/admin/views/EntryEditor/AddressEditor.vue
+++ resources/assets/admin/views/EntryEditor/AddressEditor.vue
@@ -2,7 +2,7 @@
     <div class="addresss_editor">
         <div v-for="(eachField, fieldKey) in field.raw.fields" class="each_address_field" :key="fieldKey">
             <label>{{getLaebel(eachField)}}</label>
-            <el-input type="text" size="mini" v-model="value[fieldKey]"></el-input>
+            <el-input type="text" size="mini" v-model="model[fieldKey]"></el-input>
         </div>
     </div>
 </template>
@@ -11,14 +11,24 @@
     export default {
         name: 'AddressEditor',
         props: ['value', 'field'],
+	    data() {
+		    return {
+			    model: this.value
+		    }
+	    },
+	    watch: {
+		    model() {
+			    this.$emit('input', this.model);
+		    }
+	    },
         methods: {
           getLaebel(field) {
               return field.settings.admin_field_label || field.settings.label || field.attributes.name;
           }
         },
         created() {
-            if(!this.value) {
-                this.value = {};
+            if (!this.model || Array.isArray(this.model)) {
+                this.model = {};
             }
         }
     }
