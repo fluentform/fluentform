0830deda78ff901189b19742a40902c74d403a0d
Dhrupo Nil <dhrupo@gmail.com>
2024-03-04 10:41:42 +0600

    Fix: conditional logic supports number formatting
    https://support.wpmanageninja.com/#/tickets/86922/view

--- app/Services/ConditionAssesor.php
+++ app/Services/ConditionAssesor.php
@@ -52,6 +52,10 @@ class ConditionAssesor
             }
             $inputValue = Arr::get($inputs, $accessor);
 
+            if ($numericFormatter = Arr::get($conditional, 'numeric_formatter')) {
+                $inputValue = self::numberFormatting($numericFormatter, $inputValue);
+            }
+
             switch ($conditional['operator']) {
                 case '=':
                     if(is_array($inputValue)) {
@@ -121,5 +125,47 @@ class ConditionAssesor
         }
 
         return false;
+    }
+
+    /**
+     * @param $numericFormatter
+     * @param $inputValue
+     *
+     * @return false|float|int|mixed
+     */
+    public static function numberFormatting($numericFormatter, $inputValue)
+    {
+        if ($numericFormatter == 'comma_dot_style') {
+            $parsedNumber = str_replace(',', '', $inputValue);
+            $locale = 'en_US';
+            $decimalSeparator = '.';
+            $groupingSeparator = ',';
+            $fractionDigits = 2;
+        } elseif ($numericFormatter == 'dot_comma_style_zero') {
+            $parsedNumber = str_replace(',', '', $inputValue);
+            $locale = 'en_US';
+            $decimalSeparator = '.';
+            $groupingSeparator = ',';
+            $fractionDigits = 0;
+        } elseif ($numericFormatter == 'dot_comma_style') {
+            $parsedNumber = str_replace('.', '', $inputValue);
+            $locale = 'de_DE';
+            $decimalSeparator = ',';
+            $groupingSeparator = '.';
+            $fractionDigits = 2;
+        } elseif ($numericFormatter == 'comma_dot_style_zero') {
+            $parsedNumber = str_replace('.', '', $inputValue);
+            $locale = 'de_DE';
+            $decimalSeparator = ',';
+            $groupingSeparator = '.';
+            $fractionDigits = 0;
+        }
+
+        $formatter = new \NumberFormatter($locale, \NumberFormatter::DECIMAL);
+        $formatter->setAttribute(\NumberFormatter::DECIMAL_SEPARATOR_SYMBOL, $decimalSeparator);
+        $formatter->setAttribute(\NumberFormatter::GROUPING_SEPARATOR_SYMBOL, $groupingSeparator);
+        $formatter->setAttribute(\NumberFormatter::FRACTION_DIGITS, $fractionDigits);
+
+        return $formatter->parse($parsedNumber);
     }
 }
