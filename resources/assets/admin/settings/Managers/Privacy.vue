c375c23ae3ccd288f2c4c31b53db07f1d6b3b0a1
Dhrupo Nil <dhrupo@gmail.com>
2024-03-25 12:55:53 +0600

    Fix: typo in Managers page
    https://trello.com/c/7D7jKhrI/2824-typo-issue

--- resources/assets/admin/settings/Managers/Privacy.vue
+++ resources/assets/admin/settings/Managers/Privacy.vue
@@ -3,7 +3,7 @@
     <div class="ff_block_item" v-if="roles.length">
         <el-skeleton :loading="loading" animated :rows="4">
             <h6 class="ff_block_title mb-1">{{ $t('Role Based') }}</h6>
-            <p class="ff_block_text">{{ $t('Administrators have full access to Fluent Forms.By selecting additional roles bellow, you can give access to other user roles.') }}</p>
+            <p class="ff_block_text">{{ $t('Administrators have full access to Fluent Forms. By selecting additional roles below, you can give access to other user roles.') }}</p>
             <div class="ff_block_item_body mt-3">
                 <div class="mb-3">
                     <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">
