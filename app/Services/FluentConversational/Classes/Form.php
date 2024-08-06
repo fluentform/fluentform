fd80427af32932ba374c4f800df272c6be231d9a
Dhrupo Nil <dhrupo@gmail.com>
2024-08-06 15:49:09 +0600

    Fix: use theme default 404 page if exists

--- app/Services/FluentConversational/Classes/Form.php
+++ app/Services/FluentConversational/Classes/Form.php
@@ -652,7 +652,8 @@ class Form
             global $wp_query;
             $wp_query->set_404();
             status_header(404);
-            get_template_part(404);
+            nocache_headers();
+            include(get_query_template('404'));
             exit();
         }
 
