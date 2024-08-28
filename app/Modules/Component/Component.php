e41d7bc5a8d0e222258a3a6cf55c3baca019a231
Lukman Nakib <lukman.nakib@gmail.com>
2024-08-28 15:57:20 +0600

    Update: improved condition event with debounce on change

--- app/Modules/Component/Component.php
+++ app/Modules/Component/Component.php
@@ -1209,6 +1209,9 @@ class Component
         ];
 
         if ($formBuilder->conditions || array_intersect($formBuilder->fieldLists, $advancedFields)) {
+            wp_localize_script('fluentform-advanced', 'fluentform_advanced_vars', [
+                'debounce_time' => 300,
+            ]);
             wp_enqueue_script('fluentform-advanced');
         }
     }
--- resources/assets/public/Pro/form-conditionals.js
+++ resources/assets/public/Pro/form-conditionals.js
@@ -43,15 +43,27 @@ const formConditional = function ($, $theForm, form) {
             const conditionAppInstance = new ConditionApp(form.conditionals, formData);
 
             $.each(watchableFields, (name, el) => {
-                el.on('change', () => {
+                el.on('keyup change', () => {
                     formData = getFormData();
                     conditionAppInstance.setFormData(formData);
-                    hideShowElements(conditionAppInstance.getCalculatedStatuses());
+                    debouncedHideShowElements(conditionAppInstance.getCalculatedStatuses());
                 });
             });
 
             hideShowElements(conditionAppInstance.getCalculatedStatuses());
         };
+
+        const debounce = (func, delay = 300) => {
+            let timeoutId;
+            return (...args) => {
+                clearTimeout(timeoutId);
+                timeoutId = setTimeout(() => func.apply(this, args), delay);
+            };
+        };
+
+        const debouncedHideShowElements = debounce((statuses) => {
+            hideShowElements(statuses);
+        }, window.fluentform_advanced_vars.debounce_time);
 
         const hideShowElements = function (items) {
             $.each(items, (itemName, status) => {
