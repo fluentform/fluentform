0d4e96e90a93c64f7235969216e6f86fdfed020c
Lukman Nakib <lukman.nakib@gmail.com>
2024-11-26 11:50:17 +0600

    Adds note in entry export

--- app/Services/Transfer/TransferService.php
+++ app/Services/Transfer/TransferService.php
@@ -10,6 +10,7 @@ use FluentForm\App\Models\Submission;
 use FluentForm\App\Modules\Form\FormDataParser;
 use FluentForm\App\Modules\Form\FormFieldsParser;
 use FluentForm\App\Services\FormBuilder\ShortCodeParser;
+use FluentForm\App\Services\Submission\SubmissionService;
 use FluentForm\Framework\Foundation\App;
 use FluentForm\Framework\Request\File;
 use FluentForm\Framework\Support\Arr;
@@ -112,13 +113,7 @@ class TransferService
                             }
                         }
                     }
-                    do_action_deprecated('fluentform_form_imported', [
-                            $formId
-                        ],
-                        FLUENTFORM_FRAMEWORK_UPGRADE,
-                        'fluentform/form_imported',
-                        'Use fluentform/form_imported instead of fluentform_form_imported.'
-                    );
+               
                     do_action('fluentform/form_imported', $formId);
                 }
 
@@ -159,6 +154,8 @@ class TransferService
             $selectedLabels = \json_decode($selectedLabels, true);
         }
         $selectedLabels = fluentFormSanitizer($selectedLabels);
+    
+        $withNotes = isset($args['with_notes']);
        
         //filter out unselected fields
         foreach ($inputLabels as $key => $value) {
@@ -166,15 +163,16 @@ class TransferService
                 unset($inputLabels[$key]); // Remove the element with the specified key
             }
         }
-    
+        
         $submissions = self::getSubmissions($args);
         $submissions = FormDataParser::parseFormEntries($submissions, $form, $formInputs);
         $parsedShortCodes = [];
         $exportData = [];
+        $submissionService = new SubmissionService();
         foreach ($submissions as $submission) {
 
             $submission->response = json_decode($submission->response, true);
-           
+         
             $temp = [];
             foreach ($inputLabels as $field => $label) {
                 
@@ -213,6 +211,12 @@ class TransferService
                     }
                 }
             }
+            if($withNotes){
+                $notes = $submissionService->getNotes($submission->id, ['form_id' => $form->id])->pluck('value');
+                if(!empty($notes)){
+                    $temp[] = implode(", ",$notes->toArray());
+                }
+            }
             
             $exportData[] = $temp;
         }
@@ -224,20 +228,13 @@ class TransferService
                 $extraLabels[] = Arr::get($code,'label');
             }
         }
+        
         $inputLabels = array_merge($inputLabels, $extraLabels);
+        if($withNotes){
+            $inputLabels[] = __('Notes','fluentform');
+        }
         $data = array_merge([array_values($inputLabels)], $exportData);
-        $data = apply_filters_deprecated(
-            'fluentform_export_data',
-            [
-                $data,
-                $form,
-                $exportData,
-                $inputLabels
-            ],
-            FLUENTFORM_FRAMEWORK_UPGRADE,
-            'fluentform/export_data',
-            'Use fluentform/export_data instead of fluentform_export_data'
-        );
+        
         $data = apply_filters('fluentform/export_data', $data, $form, $exportData, $inputLabels);
         $fileName = sanitize_title($form->title, 'export', 'view') . '-' . date('Y-m-d');
         self::downloadOfficeDoc($data, $type, $fileName);
