beefbb8882b3cb494376eb9a7f5d482d934929f8
Lukman Nakib <lukman.nakib@gmail.com>
2025-01-21 16:04:55 +0600

    Improved Honeypot

--- app/Modules/Form/HoneyPot.php
+++ app/Modules/Form/HoneyPot.php
@@ -19,22 +19,46 @@ class HoneyPot
         if (!$this->isEnabled($form->id)) {
             return;
         }
+    
+        $fieldName = $this->getFieldName($form->id);
+        $fieldId = 'ff_' . $form->id . '_item_sf' ;
+        $randomClass = 'form_field_' . rand(10, 99) .'_item_sf';
+    
+        $labels = ['Newsletter', 'Updates', 'Contact', 'Subscribe', 'Notify'];
+        $randomLabel = $labels[array_rand($labels)];
         ?>
-        <span style="display: none !important;"><input type="checkbox"
-                name="<?php echo esc_attr($this->getFieldName($form->id)); ?>"
-                value="1" style="display:none !important;" tabindex="-1" aria-hidden="true"></span>
+        <div class="ff-el-group <?php echo esc_attr($randomClass); ?>">
+            <div class="ff-el-input--label asterisk-right">
+                <label for="<?php echo esc_attr($fieldId); ?>" aria-label="<?php echo esc_attr($randomLabel); ?>">
+                    <?php echo esc_html($randomLabel); ?>
+                </label>
+            </div>
+            <div class="ff-el-input--content">
+                <input type="text"
+                       name="<?php echo esc_attr($fieldName); ?>"
+                       class="ff-el-form-control"
+                       id="<?php echo esc_attr($fieldId); ?>"
+                >
+            </div>
+        </div>
+        <style>
+            .<?php echo esc_attr($randomClass); ?> {
+                display: none!important;
+                position: absolute!important;
+                transform: translateX(1000%)!important;
+            }
+        </style>
         <?php
+        
     }
-
+    
     public function verify($insertData, $requestData, $formId)
     {
         if (!$this->isEnabled($formId)) {
             return;
         }
 
-        // Now verify
         if (ArrayHelper::get($requestData, $this->getFieldName($formId))) {
-            // It's a bot! Block him
             wp_send_json(
                 [
                     'errors' => 'Sorry! You can not submit this form at this moment!',
@@ -42,43 +66,20 @@ class HoneyPot
                 422
             );
         }
-
         return;
     }
-
+    
     public function isEnabled($formId = false)
     {
         $option = get_option('_fluentform_global_form_settings');
         $status = 'yes' == ArrayHelper::get($option, 'misc.honeypotStatus');
-    
-        $status = apply_filters_deprecated(
-            'fluentform_honeypot_status',
-            [
-                $status,
-                $formId
-            ],
-            FLUENTFORM_FRAMEWORK_UPGRADE,
-            'fluentform/honeypot_status',
-            'Use fluentform/honeypot_status instead of fluentform_honeypot_status.'
-        );
-
+        
         return apply_filters('fluentform/honeypot_status', $status, $formId);
     }
-
+    
     private function getFieldName($formId)
     {
-        $honeyPotName = 'item__' . $formId . '__fluent_checkme_';
-        $honeyPotName =  apply_filters_deprecated(
-            'fluentform_honeypot_name',
-            [
-                $honeyPotName,
-                $formId
-            ],
-            FLUENTFORM_FRAMEWORK_UPGRADE,
-            'fluentform/honeypot_name',
-            'Use fluentform/honeypot_name instead of fluentform_honeypot_name.'
-        );
-
+        $honeyPotName = 'item_' . $formId . '__fluent_sf';
         return apply_filters('fluentform/honeypot_name', $honeyPotName, $formId);
     }
 }
