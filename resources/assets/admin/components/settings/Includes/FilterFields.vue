4cbf388d308becb5d8cb62fae842791a5e5e54c6
Lukman Nakib <lukman.nakib@gmail.com>
2025-02-24 17:51:29 +0600

    Update: UI of Conditional Groups in Settings

--- resources/assets/admin/components/settings/Includes/FilterFields.vue
+++ resources/assets/admin/components/settings/Includes/FilterFields.vue
@@ -24,140 +24,153 @@
 
 	        <div v-if="conditionals.type === 'group'" class="ff_conditions_warp">
 		        <div v-for="(group, groupIndex) in items" :key="groupIndex" class="group-container" :class="{'is-empty': isGroupEmpty(group)}">
-			        <el-row class="group-header items-center" >
-				        <el-col :md="11" class="mb-1">
-					        <div class="title-section">
-						        <template v-if="group.isEditingTitle">
-							        <el-input
-								        v-model="group.title"
-								        class="title-input"
-								        @blur="finishTitleEdit(group)"
-								        @keyup.enter.native="finishTitleEdit(group)"
-							        />
-						        </template>
-						        <template v-else>
-							        <div class="group-title" @click="startTitleEdit(group)">
-								        <span v-if="group.title">{{ group.title }}</span>
-								        <span v-else>{{ `${$t('Group')} ${groupIndex + 1}` }}</span>
-								        <i class="el-icon-edit-outline" style="font-size: 12px; margin-left: 5px;"></i>
-							        </div>
-						        </template>
-					        </div>
-				        </el-col>
-				        <el-col :md="8" class="mb-1">
-					        <div class="group-relationship">
-						        <b> {{ groupIndex === 0 ? $t('IF') : $t('OR IF') }} </b>
-					        </div>
-				        </el-col>
-				        <el-col :md="4" class="mt-1">
-					        <div class="actions">
-						        <el-button v-if="items.length > 1" @click="removeGroup(groupIndex)" icon="el-icon-delete" type="danger" plain>
-						        </el-button>
-						        <el-button v-if="items.length > 1" @click="toggleGroup(group)" plain>
-							        <i :class="[
+			        <div class="group-items">
+                        <el-row class="group-header items-center" >
+                            <el-col :md="12" class="mb-1">
+                                <div class="title-section">
+                                    <div class="group-relationship">
+                                        <b> {{ $t('IF')}} </b>
+                                    </div>
+
+                                    <template v-if="group.isEditingTitle">
+                                        <el-input
+                                                v-model="group.title"
+                                                class="title-input"
+                                                @blur="finishTitleEdit(group)"
+                                                @keyup.enter.native="finishTitleEdit(group)"
+                                        />
+                                    </template>
+                                    <template v-else>
+                                        <div class="group-title" @click="startTitleEdit(group)">
+                                            <span v-if="group.title">{{ group.title }}</span>
+                                            <span v-else>{{ `${$t('Group')} ${groupIndex + 1}` }}</span>
+                                            <i class="el-icon-edit-outline" style="font-size: 12px; margin-left: 5px;"></i>
+                                        </div>
+                                    </template>
+                                </div>
+                            </el-col>
+
+                            <el-col :md="12" class=" mb-0">
+                                <div class="actions pull-right">
+
+                                    <el-button class="el-button--icon"  size="mini" v-if="items.length > 1" @click="removeGroup(groupIndex)" icon="el-icon-delete" type="danger" >
+                                    </el-button>
+                                    <el-button class="el-button--icon"   size="mini" v-if="items.length > 1" @click="toggleGroup(group)" plain>
+                                        <i :class="[
 	                                    { 'el-icon-arrow-up': group.isGroupOpen },
 	                                    { 'el-icon-arrow-down': !group.isGroupOpen }
 	                                ]"></i>
-						        </el-button>
-				            </div>
-				        </el-col>
-			        </el-row>
+                                    </el-button>
+                                </div>
+                            </el-col>
+                        </el-row>
 
-			        <el-row v-for="(logic, ruleIndex) in group.rules" :key="`${groupIndex}-${ruleIndex}`" :gutter="5" class="items-center conditional-logic" v-show="group.isGroupOpen">
-				        <el-col :md="8" class="mb-2">
-					        <div>
-						        <el-select
-							        :placeholder="$t('Select')"
-							        popper-class="ff-mw-100"
-							        v-model="logic.field"
-							        style="width: 100%" @change="logic.value = ''">
-							        <el-option
-								        v-for="(field, key) in fields" :key="key"
-								        :label="field.admin_label" :value="key"
-							        ></el-option>
-						        </el-select>
-					        </div>
-				        </el-col>
+                        <el-row v-for="(logic, ruleIndex) in group.rules" :key="`${groupIndex}-${ruleIndex}`" :gutter="5" class="items-center conditional-logic" v-show="group.isGroupOpen">
+                            <el-col :md="8" class="mb-0">
+                                <div>
+                                    <el-select
+                                            size="small"
+                                            :placeholder="$t('Select')"
+                                            popper-class="ff-mw-100"
+                                            v-model="logic.field"
+                                            style="width: 100%" @change="logic.value = ''">
+                                        <el-option
+                                                v-for="(field, key) in fields" :key="key"
+                                                :label="field.admin_label" :value="key"
+                                        ></el-option>
+                                    </el-select>
+                                </div>
+                            </el-col>
 
 
-				        <el-col :md="5" class="mb-2">
-					        <div>
-						        <el-select :placeholder="$t('Select')"  v-model="logic.operator">
-							        <el-option-group :label="$t('General Operators')">
-								        <el-option value="=" :label="$t('equal')"></el-option>
-								        <el-option value="!=" :label="$t('not equal')"></el-option>
-								        <template v-if="fields[logic.field] && !Object.keys(fields[logic.field].options || {}).length">
-									        <el-option value=">" :label="$t('greater than')"></el-option>
-									        <el-option value="<" :label="$t('less than')"></el-option>
-									        <el-option value=">=" :label="$t('greater than or equal')"></el-option>
-									        <el-option value="<=" :label="('less than or equal')"></el-option>
-									        <el-option value="contains" :label="$t('contains')"></el-option>
-									        <el-option value="doNotContains" :label="$t('do not contains')"></el-option>
-									        <el-option value="startsWith" :label="$t('starts with')"></el-option>
-									        <el-option value="endsWith" :label="$t('ends with')"></el-option>
-								        </template>
-							        </el-option-group>
-							        <el-option-group :label="$t('Advanced Operators')">
-								        <el-option value="length_equal" :label="$t('Equal to Data Length')"></el-option>
-								        <el-option value="length_less_than" :label="$t('Less than to Data length')"></el-option>
-								        <el-option value="length_greater_than" :label="$t('Greater than to Data Length')"></el-option>
-								        <el-option value="test_regex" :label="$t('Regex Match')"></el-option>
-							        </el-option-group>
-						        </el-select>
-					        </div>
-				        </el-col>
+                            <el-col :md="5" class="mb-0">
+                                <div>
+                                    <el-select                                         size="small"
+                                                                                       :placeholder="$t('Select')"  v-model="logic.operator">
+                                        <el-option-group :label="$t('General Operators')">
+                                            <el-option value="=" :label="$t('equal')"></el-option>
+                                            <el-option value="!=" :label="$t('not equal')"></el-option>
+                                            <template v-if="fields[logic.field] && !Object.keys(fields[logic.field].options || {}).length">
+                                                <el-option value=">" :label="$t('greater than')"></el-option>
+                                                <el-option value="<" :label="$t('less than')"></el-option>
+                                                <el-option value=">=" :label="$t('greater than or equal')"></el-option>
+                                                <el-option value="<=" :label="('less than or equal')"></el-option>
+                                                <el-option value="contains" :label="$t('contains')"></el-option>
+                                                <el-option value="doNotContains" :label="$t('do not contains')"></el-option>
+                                                <el-option value="startsWith" :label="$t('starts with')"></el-option>
+                                                <el-option value="endsWith" :label="$t('ends with')"></el-option>
+                                            </template>
+                                        </el-option-group>
+                                        <el-option-group :label="$t('Advanced Operators')">
+                                            <el-option value="length_equal" :label="$t('Equal to Data Length')"></el-option>
+                                            <el-option value="length_less_than" :label="$t('Less than to Data length')"></el-option>
+                                            <el-option value="length_greater_than" :label="$t('Greater than to Data Length')"></el-option>
+                                            <el-option value="test_regex" :label="$t('Regex Match')"></el-option>
+                                        </el-option-group>
+                                    </el-select>
+                                </div>
+                            </el-col>
 
-				        <el-col :md="8" class="mb-2">
-					        <div >
-						        <template v-if="logic.operator == 'length_equal' || logic.operator == 'length_less_than' || logic.operator == 'length_greater_than'">
-							        <el-input  type="number" step="1" :placeholder="('Enter length in number')" v-model="logic.value" />
-						        </template>
-						        <template v-else>
-							        <el-select
-								        v-if="fields[logic.field] && Object.keys(fields[logic.field].options || {}).length"
-							                   v-model="logic.value"
-								        clearable filterable allow-create
-								        style="width: 100%">
-								        <el-option
-									        v-for="(label, value) in fields[logic.field].options"
-									        :key="value"
-								                   :label="label" :value="value"
+                            <el-col :md="8" class="mb-0">
+                                <div >
+                                    <template v-if="logic.operator == 'length_equal' || logic.operator == 'length_less_than' || logic.operator == 'length_greater_than'">
+                                        <el-input                                         size="small"
+                                                                                          type="number" step="1" :placeholder="('Enter length in number')" v-model="logic.value" />
+                                    </template>
+                                    <template v-else>
+                                        <el-select
+                                                size="small"
 
-								        ></el-option>
-							        </el-select>
-							        <el-input
-								        v-else
-								        :placeholder="$t('Enter a value')"
-								        v-model="logic.value"
-							        ></el-input>
-						        </template>
-					        </div>
-				        </el-col>
+                                                v-if="fields[logic.field] && Object.keys(fields[logic.field].options || {}).length"
+                                                v-model="logic.value"
+                                                clearable filterable allow-create
+                                                style="width: 100%">
+                                            <el-option
+                                                    v-for="(label, value) in fields[logic.field].options"
+                                                    :key="value"
+                                                    :label="label" :value="value"
 
-				        <el-col :md="2" class="mb-2">
-					        <action-btn>
-						        <action-btn-add @click="addCondition(groupIndex, ruleIndex)" />
-						        <action-btn-remove v-if="group.rules.length > 1" @click="removeCondition(groupIndex, ruleIndex)" />
-					        </action-btn>
-				        </el-col>
-			        </el-row>
+                                            ></el-option>
+                                        </el-select>
+                                        <el-input
+                                                size="small"
 
-			        <div class="preview-section" v-if="!isGroupEmpty(group)">
-				        <div class="preview-header" @click="togglePreview(group)">
-					        <div class="preview-toggle">
-						        <i :class="[
+                                                v-else
+                                                :placeholder="$t('Enter a value')"
+                                                v-model="logic.value"
+                                        ></el-input>
+                                    </template>
+                                </div>
+                            </el-col>
+
+                            <el-col :md="2" class="mb-0 ">
+                                <action-btn class="pull-right">
+                                    <action-btn-remove size="mini" v-if="group.rules.length > 1" @click="removeCondition(groupIndex, ruleIndex)" />
+                                    <action-btn-add size="mini"  @click="addCondition(groupIndex, ruleIndex)" />
+                                </action-btn>
+                            </el-col>
+                        </el-row>
+
+                        <div class="preview-section"  v-if="group.rules.length > 0">
+                            <div class="preview-header" @click="togglePreview(group)">
+                                <div class="preview-toggle">
+                                    <i :class="[
                                     { 'el-icon-arrow-up': group.isPreviewOpen },
                                     { 'el-icon-arrow-down': !group.isPreviewOpen }
                                 ]"></i>
-					        </div>
-				        </div>
+                                </div>
+                            </div>
 
-				        <div v-show="group.isPreviewOpen" class="preview-content">
-					        <div class="group-preview" v-if="getGroupPreview(group)">
-						        <div class="preview-conditions" v-html="getGroupPreview(group)"></div>
-					        </div>
-				        </div>
-			        </div>
+                            <div v-show="group.isPreviewOpen" class="preview-content">
+                                <div class="group-preview" v-if="getGroupPreview(group)">
+                                    <div class="preview-conditions" v-html="getGroupPreview(group)"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div v-if="items && groupIndex !== items.length - 1" class="ff_cond_or">
+                        <span>{{ $t('OR') }}</span>
+                    </div>
 		        </div>
 
 		        <div class="add-group-btn">
@@ -167,7 +180,7 @@
                         size="small"
 				        plain
 			        >
-				        {{ $t('+ Add New Group') }}
+				        {{ $t('+ OR') }}
 			        </el-button>
 		        </div>
 	        </div>
