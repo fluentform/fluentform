eb3dfa746871d01919900379571ef87d933ef1d9
Habibur Rahman Delwar <hrdelwar75@gmail.com>
2025-04-09 18:20:13 +0600

    Add: index on fluentform_submission_meta table
    https://github.com/fluentform/fluentform/issues/255

--- database/Migrations/SubmissionMeta.php
+++ database/Migrations/SubmissionMeta.php
@@ -16,6 +16,7 @@ class SubmissionMeta
         $charsetCollate = $wpdb->get_charset_collate();
 
         $table = $wpdb->prefix . 'fluentform_submission_meta';
+        $indexPrefix = $wpdb->prefix . 'ff_s_m_idx';
 
         if ($wpdb->get_var("SHOW TABLES LIKE '$table'") != $table) {
             $sql = "CREATE TABLE $table (
@@ -29,9 +30,24 @@ class SubmissionMeta
 			  `name` VARCHAR(45) NULL,
 			  `created_at` TIMESTAMP NULL,
 			  `updated_at` TIMESTAMP NULL,
-			  PRIMARY KEY (`id`)) $charsetCollate;";
+			  PRIMARY KEY (`id`),
+              INDEX `{$indexPrefix}_response_id` (`response_id`),
+              INDEX `{$indexPrefix}_form_id` (`form_id`)) $charsetCollate;";
             require_once ABSPATH . 'wp-admin/includes/upgrade.php';
             dbDelta($sql);
+        } else {
+            // For existing installations, add indexes if they don't exist
+            $indexes = $wpdb->get_results("SHOW INDEX FROM $table");
+            $indexedColumns = [];
+            foreach ($indexes as $index) {
+                $indexedColumns[] = $index->Column_name;
+            }
+            if (!in_array('response_id', $indexedColumns)) {
+                $wpdb->query("ALTER TABLE {$table} ADD INDEX `{$indexPrefix}_response_id` (`response_id`);");
+            }
+            if (!in_array('form_id', $indexedColumns)) {
+                $wpdb->query("ALTER TABLE {$table} ADD INDEX `{$indexPrefix}_form_id` (`form_id`);");
+            }
         }
     }
 }
