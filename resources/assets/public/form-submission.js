7f829cf33894a66e5ae4d8a5c1b1bb8116490da0
Habibur Rahman Delwar <hrdelwar75@gmail.com>
2025-10-09 10:00:15 +0600

    Add: form submission custom js event

--- resources/assets/public/form-submission.js
+++ resources/assets/public/form-submission.js
@@ -336,6 +336,15 @@ jQuery(document).ready(function () {
                                 response: res
                             });
 
+                            const customSuccessEvent = new CustomEvent('fluentform_submission_success', {
+                                detail: {
+                                    form: $theForm[0],
+                                    config: form,
+                                    response: res
+                                }
+                            });
+                            document.dispatchEvent(customSuccessEvent);
+
                             if ('redirectUrl' in res.data.result) {
                                 if (res.data.result.message) {
                                     $('<div/>', {
@@ -395,6 +404,15 @@ jQuery(document).ready(function () {
                                 form: $theForm,
                                 response: res
                             });
+
+                            const customFailedEvent = new CustomEvent('fluentform_submission_failed', {
+                                detail: {
+                                    form: $theForm[0],
+                                    response: res,
+                                    config: form
+                                }
+                            });
+                            document.dispatchEvent(customFailedEvent);
 
                             if (!res || !res.responseJSON || !res.responseJSON || !res.responseJSON.errors) {
                                 showErrorMessages(res.responseText);
