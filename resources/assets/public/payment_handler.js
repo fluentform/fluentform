2d9c2997cbeda269792403164672f50b152d82c7
Dhrupo Nil <dhrupo@gmail.com>
2025-10-13 11:28:07 +0600

    Update: remove unwanted accessbility code for stripe inline

--- resources/assets/public/payment_handler.js
+++ resources/assets/public/payment_handler.js
@@ -16,7 +16,6 @@ export class Payment_handler {
     init() {
         this.boot();
         this.initStripeElement();
-        this.handleAccessibility();
     }
 
     $t(stringKey) {
@@ -798,24 +797,6 @@ export class Payment_handler {
         setTimeout(() => {
             this.maybeRemoveSubmitError();
         }, 500)
-    }
-
-    handleAccessibility() {
-        const observer = new MutationObserver(() => {
-            document.querySelectorAll("iframe").forEach(iframe => {
-                if (!iframe.hasAttribute("title")) {
-                    // Stripe iframe
-                    if (iframe.name.startsWith("__privateStripeFrame")) {
-                        iframe.setAttribute("title", this.$t("Secure Stripe Payment Frame"));
-                    }
-                    if (iframe.name.startsWith('single-card')) {
-                        iframe.setAttribute("title", this.$t("Secure Square Payment Frame"));
-                    }
-                }
-            });
-        });
-
-        observer.observe(document.body, { childList: true, subtree: true });
     }
 
     stripeSetupIntent(data) {
