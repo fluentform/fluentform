0f8a5b7ca035e5adf4ef753ea598d8364439ff6e
Lukman Nakib <lukman.nakib@gmail.com>
2025-11-07 18:25:42 +0600

    Feature : Presets for some controls

--- guten_block/src/components/controls/FluentBorderControl.js
+++ guten_block/src/components/controls/FluentBorderControl.js
@@ -1,5 +1,5 @@
-const { BaseControl, ToggleControl, SelectControl } = wp.components;
-const { useEffect, memo, useCallback, useRef } = wp.element;
+const { BaseControl, ToggleControl, SelectControl, Button } = wp.components;
+const { useEffect, memo, useCallback, useRef, useState } = wp.element;
 const { __ } = wp.i18n;
 import FluentColorPicker from "./FluentColorPicker";
 import FluentSpaceControl from "./FluentSpaceControl";
@@ -11,6 +11,8 @@ const FluentBorderControl = ({
     onChange,
     defaultColor = "#dddddd"
 }) => {
+    const [showPresets, setShowPresets] = useState(false);
+
     // Use internal state to track the border object
     const currentBorderRef = useRef(border || {
         enable: false,
@@ -39,36 +41,191 @@ const FluentBorderControl = ({
         { label: __("Double"), value: 'double' }
     ];
 
-    return (
-        <BaseControl label={label}>
-            <ToggleControl
-                label={__("Enable Border")}
-                checked={border.enable}
-                onChange={(value) => {
-                    const updates = { enable: value };
+    // Border presets
+    const borderPresets = [
+        {
+            label: __("None"),
+            value: {
+                enable: false,
+                type: 'solid',
+                color: '',
+                width: {
+                    desktop: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true },
+                    tablet: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true },
+                    mobile: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true }
+                },
+                radius: {
+                    desktop: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true },
+                    tablet: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true },
+                    mobile: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true }
+                }
+            }
+        },
+        {
+            label: __("Thin"),
+            value: {
+                enable: true,
+                type: 'solid',
+                color: '#dddddd',
+                width: {
+                    desktop: { unit: 'px', top: '1', right: '1', bottom: '1', left: '1', linked: true },
+                    tablet: { unit: 'px', top: '1', right: '1', bottom: '1', left: '1', linked: true },
+                    mobile: { unit: 'px', top: '1', right: '1', bottom: '1', left: '1', linked: true }
+                },
+                radius: {
+                    desktop: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true },
+                    tablet: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true },
+                    mobile: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true }
+                }
+            }
+        },
+        {
+            label: __("Medium"),
+            value: {
+                enable: true,
+                type: 'solid',
+                color: '#dddddd',
+                width: {
+                    desktop: { unit: 'px', top: '2', right: '2', bottom: '2', left: '2', linked: true },
+                    tablet: { unit: 'px', top: '2', right: '2', bottom: '2', left: '2', linked: true },
+                    mobile: { unit: 'px', top: '2', right: '2', bottom: '2', left: '2', linked: true }
+                },
+                radius: {
+                    desktop: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true },
+                    tablet: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true },
+                    mobile: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true }
+                }
+            }
+        },
+        {
+            label: __("Rounded"),
+            value: {
+                enable: true,
+                type: 'solid',
+                color: '#dddddd',
+                width: {
+                    desktop: { unit: 'px', top: '1', right: '1', bottom: '1', left: '1', linked: true },
+                    tablet: { unit: 'px', top: '1', right: '1', bottom: '1', left: '1', linked: true },
+                    mobile: { unit: 'px', top: '1', right: '1', bottom: '1', left: '1', linked: true }
+                },
+                radius: {
+                    desktop: { unit: 'px', top: '5', right: '5', bottom: '5', left: '5', linked: true },
+                    tablet: { unit: 'px', top: '5', right: '5', bottom: '5', left: '5', linked: true },
+                    mobile: { unit: 'px', top: '5', right: '5', bottom: '5', left: '5', linked: true }
+                }
+            }
+        },
+        {
+            label: __("Pill"),
+            value: {
+                enable: true,
+                type: 'solid',
+                color: '#dddddd',
+                width: {
+                    desktop: { unit: 'px', top: '1', right: '1', bottom: '1', left: '1', linked: true },
+                    tablet: { unit: 'px', top: '1', right: '1', bottom: '1', left: '1', linked: true },
+                    mobile: { unit: 'px', top: '1', right: '1', bottom: '1', left: '1', linked: true }
+                },
+                radius: {
+                    desktop: { unit: 'px', top: '50', right: '50', bottom: '50', left: '50', linked: true },
+                    tablet: { unit: 'px', top: '50', right: '50', bottom: '50', left: '50', linked: true },
+                    mobile: { unit: 'px', top: '50', right: '50', bottom: '50', left: '50', linked: true }
+                }
+            }
+        },
+        {
+            label: __("Dashed"),
+            value: {
+                enable: true,
+                type: 'dashed',
+                color: '#dddddd',
+                width: {
+                    desktop: { unit: 'px', top: '2', right: '2', bottom: '2', left: '2', linked: true },
+                    tablet: { unit: 'px', top: '2', right: '2', bottom: '2', left: '2', linked: true },
+                    mobile: { unit: 'px', top: '2', right: '2', bottom: '2', left: '2', linked: true }
+                },
+                radius: {
+                    desktop: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true },
+                    tablet: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true },
+                    mobile: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true }
+                }
+            }
+        }
+    ];
 
-                    if (value) {
-                        if (!border.color) updates.color = defaultColor;
-                        if (!border.type) updates.type = 'solid';
-                        if (!border.width || Object.keys(border.width).length === 0) {
-                            updates.width = {
-                                desktop: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true },
-                                tablet: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true },
-                                mobile: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true }
-                            };
+    const applyPreset = (preset) => {
+        updateBorder(preset.value);
+        setShowPresets(false);
+    };
+
+    return (
+        <BaseControl label={label} className="ffblock-border-control">
+            <div className="ffblock-control-header">
+                <ToggleControl
+                    label={__("Enable Border")}
+                    checked={border.enable}
+                    onChange={(value) => {
+                        const updates = { enable: value };
+
+                        if (value) {
+                            if (!border.color) updates.color = defaultColor;
+                            if (!border.type) updates.type = 'solid';
+                            if (!border.width || Object.keys(border.width).length === 0) {
+                                updates.width = {
+                                    desktop: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true },
+                                    tablet: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true },
+                                    mobile: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true }
+                                };
+                            }
+                            if (!border.radius || Object.keys(border.radius).length === 0) {
+                                updates.radius = {
+                                    desktop: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true },
+                                    tablet: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true },
+                                    mobile: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true }
+                                };
+                            }
                         }
-                        if (!border.radius || Object.keys(border.radius).length === 0) {
-                            updates.radius = {
-                                desktop: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true },
-                                tablet: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true },
-                                mobile: { unit: 'px', top: '', right: '', bottom: '', left: '', linked: true }
-                            };
-                        }
-                    }
-                    
-                    updateBorder(updates);
-                }}
-            />
+
+                        updateBorder(updates);
+                    }}
+                />
+                <Button
+                    icon="grid-view"
+                    isSmall
+                    onClick={() => setShowPresets(!showPresets)}
+                    className="ffblock-preset-toggle"
+                    label={__("Border Presets")}
+                />
+            </div>
+
+            {showPresets && (
+                <div className="ffblock-presets-container">
+                    <div className="ffblock-presets-grid">
+                        {borderPresets.map((preset, index) => (
+                            <Button
+                                key={index}
+                                className="ffblock-preset-button"
+                                onClick={() => applyPreset(preset)}
+                            >
+                                <div className="ffblock-preset-preview ffblock-border-preview">
+                                    <div
+                                        className="ffblock-border-box"
+                                        style={{
+                                            border: preset.value.enable
+                                                ? `${preset.value.width.desktop.top || 0}px ${preset.value.type} ${preset.value.color}`
+                                                : 'none',
+                                            borderRadius: preset.value.enable && preset.value.radius.desktop.top
+                                                ? `${preset.value.radius.desktop.top}px`
+                                                : '0'
+                                        }}
+                                    />
+                                </div>
+                                <span className="ffblock-preset-label">{preset.label}</span>
+                            </Button>
+                        ))}
+                    </div>
+                </div>
+            )}
 
             {border.enable && (
                 <>
@@ -90,12 +247,14 @@ const FluentBorderControl = ({
                         label={__("Border Width")}
                         values={border.width}
                         onChange={(value) => updateBorder({ width: value })}
+                        showPresetsToggle={false}
                     />
 
                     <FluentSpaceControl
                         label={__("Border Radius")}
                         values={border.radius}
                         onChange={(value) => updateBorder({ radius: value })}
+                        showPresetsToggle={false}
                     />
                 </>
             )}
--- guten_block/src/components/controls/FluentBoxShadowControl.js
+++ guten_block/src/components/controls/FluentBoxShadowControl.js
@@ -1,5 +1,5 @@
-const { BaseControl, ToggleControl, SelectControl } = wp.components;
-const { useEffect, memo, useCallback, useRef } = wp.element;
+const { BaseControl, ToggleControl, SelectControl, Button, ButtonGroup } = wp.components;
+const { useEffect, memo, useCallback, useRef, useState } = wp.element;
 const { __ } = wp.i18n;
 import FluentColorPicker from "./FluentColorPicker";
 import { arePropsEqual } from '../utils/ComponentUtils';
@@ -10,6 +10,8 @@ const FluentBoxShadowControl = ({
     onChange,
     defaultColor = "rgba(0,0,0,0.5)"
 }) => {
+    const [showPresets, setShowPresets] = useState(false);
+
     const shadowRef = useRef(shadow || {
         enable: false,
         color: '',
@@ -43,24 +45,140 @@ const FluentBoxShadowControl = ({
         { label: '%', value: '%' }
     ];
 
+    // Shadow presets
+    const shadowPresets = [
+        {
+            label: __("None"),
+            value: {
+                enable: false,
+                color: '',
+                position: 'outline',
+                horizontal: { value: '0', unit: 'px' },
+                vertical: { value: '0', unit: 'px' },
+                blur: { value: '0', unit: 'px' },
+                spread: { value: '0', unit: 'px' },
+            }
+        },
+        {
+            label: __("Subtle"),
+            value: {
+                enable: true,
+                color: 'rgba(0,0,0,0.1)',
+                position: 'outline',
+                horizontal: { value: '0', unit: 'px' },
+                vertical: { value: '2', unit: 'px' },
+                blur: { value: '4', unit: 'px' },
+                spread: { value: '0', unit: 'px' },
+            }
+        },
+        {
+            label: __("Small"),
+            value: {
+                enable: true,
+                color: 'rgba(0,0,0,0.15)',
+                position: 'outline',
+                horizontal: { value: '0', unit: 'px' },
+                vertical: { value: '4', unit: 'px' },
+                blur: { value: '6', unit: 'px' },
+                spread: { value: '0', unit: 'px' },
+            }
+        },
+        {
+            label: __("Medium"),
+            value: {
+                enable: true,
+                color: 'rgba(0,0,0,0.2)',
+                position: 'outline',
+                horizontal: { value: '0', unit: 'px' },
+                vertical: { value: '6', unit: 'px' },
+                blur: { value: '12', unit: 'px' },
+                spread: { value: '0', unit: 'px' },
+            }
+        },
+        {
+            label: __("Large"),
+            value: {
+                enable: true,
+                color: 'rgba(0,0,0,0.25)',
+                position: 'outline',
+                horizontal: { value: '0', unit: 'px' },
+                vertical: { value: '10', unit: 'px' },
+                blur: { value: '20', unit: 'px' },
+                spread: { value: '0', unit: 'px' },
+            }
+        },
+        {
+            label: __("Inset"),
+            value: {
+                enable: true,
+                color: 'rgba(0,0,0,0.15)',
+                position: 'inset',
+                horizontal: { value: '0', unit: 'px' },
+                vertical: { value: '2', unit: 'px' },
+                blur: { value: '4', unit: 'px' },
+                spread: { value: '0', unit: 'px' },
+            }
+        }
+    ];
+
+    const applyPreset = (preset) => {
+        updateShadow(preset.value);
+        setShowPresets(false);
+    };
+
     return (
-        <BaseControl label={label}>
-            <ToggleControl
-                label={__("Enable Box Shadow")}
-                checked={shadow.enable}
-                onChange={(value) => {
-                    const updates = { enable: value };
-                    if (value) {
-                        if (!shadow.color) updates.color = defaultColor;
-                        if (!shadow.position) updates.position = 'outline';
-                        if (!shadow.horizontal?.value) updates.horizontal = { value: '0', unit: 'px' };
-                        if (!shadow.vertical?.value) updates.vertical = { value: '0', unit: 'px' };
-                        if (!shadow.blur?.value) updates.blur = { value: '5', unit: 'px' };
-                        if (!shadow.spread?.value) updates.spread = { value: '0', unit: 'px' };
-                    }
-                    updateShadow(updates);
-                }}
-            />
+        <BaseControl label={label} className="ffblock-box-shadow-control">
+            <div className="ffblock-control-header">
+                <ToggleControl
+                    label={__("Enable Box Shadow")}
+                    checked={shadow.enable}
+                    onChange={(value) => {
+                        const updates = { enable: value };
+                        if (value) {
+                            if (!shadow.color) updates.color = defaultColor;
+                            if (!shadow.position) updates.position = 'outline';
+                            if (!shadow.horizontal?.value) updates.horizontal = { value: '0', unit: 'px' };
+                            if (!shadow.vertical?.value) updates.vertical = { value: '0', unit: 'px' };
+                            if (!shadow.blur?.value) updates.blur = { value: '5', unit: 'px' };
+                            if (!shadow.spread?.value) updates.spread = { value: '0', unit: 'px' };
+                        }
+                        updateShadow(updates);
+                    }}
+                />
+                <Button
+                    icon="grid-view"
+                    isSmall
+                    onClick={() => setShowPresets(!showPresets)}
+                    className="ffblock-preset-toggle"
+                    label={__("Shadow Presets")}
+                />
+            </div>
+
+            {showPresets && (
+                <div className="ffblock-presets-container">
+                    <div className="ffblock-presets-grid">
+                        {shadowPresets.map((preset, index) => (
+                            <Button
+                                key={index}
+                                className="ffblock-preset-button"
+                                onClick={() => applyPreset(preset)}
+                            >
+                                <div className="ffblock-preset-preview ffblock-shadow-preview">
+                                    <div
+                                        className="ffblock-shadow-box"
+                                        style={{
+                                            boxShadow: preset.value.enable
+                                                ? `${preset.value.position === 'inset' ? 'inset ' : ''}${preset.value.horizontal.value}${preset.value.horizontal.unit} ${preset.value.vertical.value}${preset.value.vertical.unit} ${preset.value.blur.value}${preset.value.blur.unit} ${preset.value.spread.value}${preset.value.spread.unit} ${preset.value.color}`
+                                                : 'none'
+                                        }}
+                                    />
+                                </div>
+                                <span className="ffblock-preset-label">{preset.label}</span>
+                            </Button>
+                        ))}
+                    </div>
+                </div>
+            )}
 
             {shadow.enable && (
                 <>
