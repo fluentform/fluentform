4db75930785b03278595d9be0da2037c2cbdc758
Dhrupo Nil <dhrupo@gmail.com>
2025-11-26 17:12:44 +0600

    Add: advanced validation smartcode parse

--- app/Services/ConditionAssesor.php
+++ app/Services/ConditionAssesor.php
@@ -4,11 +4,12 @@ namespace FluentForm\App\Services;
 
 use FluentForm\App\Helpers\Helper;
 use FluentForm\App\Helpers\Str;
+use FluentForm\App\Services\FormBuilder\EditorShortcodeParser;
 use FluentForm\Framework\Helpers\ArrayHelper as Arr;
 
 class ConditionAssesor
 {
-    public static function evaluate(&$field, &$inputs)
+    public static function evaluate(&$field, &$inputs, $form = null)
     {
         $status = Arr::get($field, 'conditionals.status');
         if (!$status) {
@@ -19,22 +20,22 @@ class ConditionAssesor
 
         // Handle group conditions
         if ($type === 'group' && $conditionGroups = Arr::get($field, 'conditionals.condition_groups')) {
-            return self::evaluateGroupConditions($conditionGroups, $inputs);
+            return self::evaluateGroupConditions($conditionGroups, $inputs, $form);
         }
 
         // Handle 'any', 'all' conditions
         if ($type !== 'group' && $conditions = Arr::get($field, 'conditionals.conditions')) {
-            return self::evaluateConditions($conditions, $inputs, $type);
+            return self::evaluateConditions($conditions, $inputs, $type, $form);
         }
         return true;
     }
 
-    private static function evaluateGroupConditions($conditionGroups, &$inputs)
+    private static function evaluateGroupConditions($conditionGroups, &$inputs, $form = null)
     {
         $hasGroupConditionsMet = true;
         foreach ($conditionGroups as $group) {
             if ($conditions = Arr::get($group, 'rules')) {
-                $hasGroupConditionsMet = self::evaluateConditions($conditions, $inputs, 'all');
+                $hasGroupConditionsMet = self::evaluateConditions($conditions, $inputs, 'all', $form);
                 if ($hasGroupConditionsMet) {
                     return true;
                 }
@@ -43,7 +44,7 @@ class ConditionAssesor
         return $hasGroupConditionsMet;
     }
 
-    private static function evaluateConditions($conditions, &$inputs, $type)
+    private static function evaluateConditions($conditions, &$inputs, $type, $form = null)
     {
         $hasConditionMet = true;
 
@@ -52,7 +53,7 @@ class ConditionAssesor
                 continue;
             }
 
-            $hasConditionMet = static::assess($condition, $inputs);
+            $hasConditionMet = static::assess($condition, $inputs, $form);
 
             if ($hasConditionMet && $type == 'any') {
                 return true;
@@ -66,7 +67,7 @@ class ConditionAssesor
         return $hasConditionMet;
     }
 
-    public static function assess(&$conditional, &$inputs)
+    public static function assess(&$conditional, &$inputs, $form = null)
     {
         if ($conditional['field']) {
             $accessor = rtrim(str_replace(['[', ']', '*'], ['.'], $conditional['field']), '.');
@@ -78,6 +79,10 @@ class ConditionAssesor
 
             if ($numericFormatter = Arr::get($conditional, 'numeric_formatter')) {
                 $inputValue = Helper::getNumericValue($inputValue, $numericFormatter);
+            }
+
+            if (isset($conditional['value']) && is_string($conditional['value'])) {
+                $conditional['value'] = self::processSmartCodesInValue($conditional['value'], $inputs, $form);
             }
 
             switch ($conditional['operator']) {
@@ -149,5 +154,49 @@ class ConditionAssesor
         }
 
         return false;
+    }
+
+    private static function processSmartCodesInValue($value, &$inputs, $form = null)
+    {
+        if (strpos($value, '{') === false) {
+            return $value;
+        }
+
+        try {
+            $processedValue = preg_replace_callback('/{+(.*?)}/', function ($matches) use ($inputs, $form) {
+                $smartCode = $matches[1];
+                
+                if (false !== strpos($smartCode, 'inputs.')) {
+                    $fieldName = substr($smartCode, strlen('inputs.'));
+                    
+                    $fieldKey = str_replace(['[', ']'], ['.', ''], $fieldName);
+                    
+                    $value = Arr::get($inputs, $fieldKey, '');
+                    
+                    if ($value === '' && $fieldKey !== $fieldName) {
+                        $value = Arr::get($inputs, $fieldName, '');
+                    }
+                    
+                    if (is_array($value)) {
+                        $value = fluentImplodeRecursive(', ', $value);
+                    }
+                    
+                    return $value !== null && $value !== '' ? $value : '';
+                }
+                
+                try {
+                    $parsed = EditorShortcodeParser::filter('{' . $smartCode . '}', $form);
+                    return $parsed;
+                } catch (\Exception $e) {
+                    // Continue to return original if parsing fails
+                }
+                
+                // Return original smart code if not handled
+                return $matches[0];
+            }, $value);
+            return $processedValue;
+        } catch (\Exception $e) {
+            return $value;
+        }
     }
 }
