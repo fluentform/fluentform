37633007a2144c8981927304810c39f973bec6ec
Lukman Nakib <lukman.nakib@gmail.com>
2025-12-26 22:29:14 +0600

    Release 6.1.12

--- fluentform.php
+++ fluentform.php
@@ -4,7 +4,7 @@ defined('ABSPATH') or die;
 /**
  * Plugin Name: Fluent Forms
  * Description: Contact Form By Fluent Forms is the advanced Contact form plugin with drag and drop, multi column supported form builder plugin
- * Version: 6.1.11
+ * Version: 6.1.12
  * Author: Contact Form - WPManageNinja LLC
  * Author URI: https://fluentforms.com
  * Plugin URI: https://wpmanageninja.com/wp-fluent-form/
@@ -17,7 +17,7 @@ defined('ABSPATH') or die;
 defined('FLUENTFORM') or define('FLUENTFORM', true);
 define('FLUENTFORM_DIR_PATH', plugin_dir_path(__FILE__));
 define('FLUENTFORM_FRAMEWORK_UPGRADE', '4.3.22');
-defined('FLUENTFORM_VERSION') or define('FLUENTFORM_VERSION', '6.1.11');
+defined('FLUENTFORM_VERSION') or define('FLUENTFORM_VERSION', '6.1.12');
 defined('FLUENTFORM_MINIMUM_PRO_VERSION') or define('FLUENTFORM_MINIMUM_PRO_VERSION', '6.0.0');
 
 if (!defined('FLUENTFORM_HAS_NIA')) {
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: contact form, wp forms, forms, form builder, custom form
 Requires at least: 6.4
 Tested up to: 6.9
 Requires PHP: 7.4
-Stable tag: 6.1.11
+Stable tag: 6.1.12
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
@@ -437,6 +437,12 @@ You can get support from our official support thread at <a href="https://wpmanag
 12. Asset Loading Comparison with Other Plugins
 
 == Changelog ==
+
+= 6.1.12 (Date: December 26, 2025) =
+- Fixes delay with condition and range slider field
+- Fixes top performing form report count
+- Fixes Button Style “Other Option” input issue with images
+- Improves security and sanitization for confirmation messages
 
 = 6.1.11 (Date: December 4, 2025) =
 - Fixes issue with saving showing invalid json
