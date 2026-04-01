10f0a9c8fe01ad2f744d8b4a717fd652dc06180c
Habibur Rahman Delwar <hrdelwar75@gmail.com>
2026-04-01 13:01:55 +0600

    Add: backward compat aliases and upgrade guide for framework v2
    - Add compat aliases for old database migration namespaces
      (FormSubmissions, SubmissionDetails) with deprecation warnings
    - Add _deprecated_function() to EntryQuery constructor
    - Replace BACKWARD_COMPAT_ENTRIES.md and DEPRECATED_CLEANUP.md
      with unified BREAKING_CHANGES_v6.2.md covering all breaking
      changes for third-party plugins
    - All deprecated items planned for removal in 7.0

---
+++ app/Compat/FormSubmissions.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FluentForm\App\Databases\Migrations;
+
+/**
+ * @deprecated since 6.2.0. Use FluentForm\Database\Migrations\Submissions instead.
+ * @todo Remove in 7.0
+ */
+class FormSubmissions extends \FluentForm\Database\Migrations\Submissions
+{
+    public function __construct()
+    {
+        _deprecated_function(__CLASS__, '6.2.0', 'FluentForm\Database\Migrations\Submissions');
+    }
+}
---
+++ app/Compat/SubmissionDetails.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FluentForm\App\Databases\Migrations;
+
+/**
+ * @deprecated since 6.2.0. Use FluentForm\Database\Migrations\SubmissionDetails instead.
+ * @todo Remove in 7.0
+ */
+class SubmissionDetails extends \FluentForm\Database\Migrations\SubmissionDetails
+{
+    public function __construct()
+    {
+        _deprecated_function(__CLASS__, '6.2.0', 'FluentForm\Database\Migrations\SubmissionDetails');
+    }
+}
