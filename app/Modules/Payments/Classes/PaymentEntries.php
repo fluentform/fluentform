09709016e70da51ecde2b6cf6cc7b73b1e3764bc
Lukman Nakib <lukman.nakib@gmail.com>
2026-04-13 13:45:55 +0600

    Fix: scope payment bulk actions to authorized forms

--- app/Modules/Payments/Classes/PaymentEntries.php
+++ app/Modules/Payments/Classes/PaymentEntries.php
@@ -123,8 +123,6 @@ class PaymentEntries
     
     public function handleBulkAction()
     {
-        Acl::verify('fluentform_forms_manager');
-
         $request = wpFluentForm()->request;
         $attributes = $request->all();
         
@@ -151,12 +149,12 @@ class PaymentEntries
         $statusCode = 400;
         // permanently delete payment entries from transactions
         if ($actionType == 'delete_items') {
-    
-            
             // get submission ids to delete order items
-            $transactionData = Transaction::select(['form_id', 'submission_id'])
+            $transactionData = Transaction::select(['id', 'form_id', 'submission_id'])
                 ->whereIn('id', $entries)
                 ->get();
+
+            $this->authorizeTransactionForms($transactionData);
 
             $submission_ids = [];
 
@@ -225,6 +223,25 @@ class PaymentEntries
         wp_send_json_success([
             'message' => $message
         ], $statusCode);
+    }
+
+    private function authorizeTransactionForms($transactionData)
+    {
+        Acl::verify('fluentform_forms_manager');
+
+        $formIds = [];
+
+        foreach ($transactionData as $transaction) {
+            $formIds[(int) $transaction->form_id] = true;
+        }
+
+        foreach (array_keys($formIds) as $formId) {
+            if (!Acl::hasPermission('fluentform_forms_manager', $formId)) {
+                wp_send_json_error([
+                    'message' => __('You do not have permission to perform this action.', 'fluentform')
+                ], 422);
+            }
+        }
     }
 
     public function getFilters()
