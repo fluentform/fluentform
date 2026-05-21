171a4401e66764ae6fc0e22a24b9829f7724d48f
Habibur Rahman Delwar <hrdelwar75@gmail.com>
2026-05-21 17:41:49 +0600

    IMPROVE: Replace SMTP nav item with suggested plugin page entry

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -394,7 +394,6 @@ class Helper
             'fluent_forms_add_ons',
             'fluent_forms_docs',
             'fluent_forms_payment_entries',
-            'fluent_forms_smtp',
             'fluent_forms_reports'
         ];
 
